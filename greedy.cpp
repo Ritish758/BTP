0ca722c3d5e9b09a29d9311d4d5e8e4028d59852
Ritish758 <ritishbansal5@gmail.com>
2022-10-27 01:52:26 +0530

    Greedy update 1.1

--- greedy.cpp
+++ greedy.cpp
@@ -33,28 +33,60 @@ bool comp(Task &a, Task &b){
 
 }
 
-int bestmatch(vector<double> &util, double req, vector<int> &time, Task t, int cur_time){
+int bestmatch(vector<int>& time, int cur_time, Task t){
+
+   int bestproc=-1;
+   
+   int larg_time=0;
+   
+   for(int i=1; i<time.size(); i++){
+      if(max(time[i],cur_time+1) + t.process_time <= t.deadline){
+          if(larg_time <= max(time[i],cur_time+1)){
+            bestproc=i;
+            larg_time=max(time[i],cur_time+1);
+          }
+      }
+   
+   }
+   
+   return bestproc;
+}
+
+int bestprocdrop(vector<vector<Task>>& proc, Task t, vector<int>& time, int cur_time){
 
  int bestproc=-1;
  
- double ma=0.0;
+ int no=1e6;
  
- double ut=1.0 - req;
- 
- //cout<<ut <<"\n";
- 
- for(int i=1; i<util.size(); i++){
-     if(max(cur_time,time[i]) + t.process_time + 1 <= t.deadline){
-     if((ut-util[i]) >= 0.000001){
-        
-        if((ma - util[i])<=0.000001){
-            ma=util[i];
-            bestproc=i;
-        
-        }
+ for(int i=1; i<proc.size(); i++){
+     vector<int> temp;
+     
+     for(auto x: proc[i]){
+       if(x.flag==0){continue;}
+       if(x.start_time==-1){temp.push_back(x.process_time);}
+       else{
+         temp.push_back(x.end_time-cur_time-1);
+       }
      }
+     
+     sort(temp.begin(),temp.end());
+     
+     int val=0;
+     int nu=0;
+     
+     for(int j=temp.size()-1; j>=0; j--){
+         val=val+temp[j];
+         nu++;
+         if(time[i]-val + t.process_time <= t.deadline){
+            if(no>nu){
+               bestproc=i;
+               no=nu;
+               break;
+            }  
+         }
      }
  }
+ 
  
  return bestproc;
 }
@@ -66,10 +98,7 @@ int batch=3;
 
 void schedule_greedy1(vector<Task>& tasks, int m){
    
-   vector<double> utilization(m+1,0);
    vector<int> time(m+1,0);
-   
-   vector<vector<double>> util_matrix;
    
    int t=0;
    int i=0;
@@ -79,7 +108,7 @@ void schedule_greedy1(vector<Task>& tasks, int m){
    
    vector<Task> intask;
    
-   queue<Task> processors[m+1];
+   vector<vector<Task>> processors(m+1);
    
    while(t<=mx){
    
@@ -99,16 +128,14 @@ void schedule_greedy1(vector<Task>& tasks, int m){
         cout<<"Task "<<x.tid<<" started at processor "<<j<<"\n";
       }
       else if(x.end_time==t){
-        double util = (double)x.process_time/(double)(x.deadline-x.arrival_time);
-        utilization[j] -= util;
         cout<<"Task "<<x.tid<<" completed at processor "<<j<<"\n";
-        processors[j].pop();
+        processors[j].erase(processors[j].begin());
         j--;
         continue;
       }
    } 
    
-   if(t%batch==0){
+   if(t%batch==0 && intask.size()>0){
      sort(intask.begin(),intask.end(),comp);
      vector<Task> not_Sch;
      for(auto x: intask){
@@ -118,25 +145,60 @@ void schedule_greedy1(vector<Task>& tasks, int m){
          cout << "Task "<<x.tid<<" Dropped\n";
          continue;
       }
-      double util = (double)(x.process_time)/(double)(x.deadline-x.arrival_time);
-      int proc=bestmatch(utilization, util, time, x, t);
+      int proc=bestmatch(time,t,x);
       //cout<< x.tid <<" "<< proc<<" "<<util<<"\n";
       if(proc!=-1){
-        processors[proc].push(x);
-        utilization[proc] += util;
+        processors[proc].push_back(x);
         cout<<"Task "<<x.tid<<" assigned to processor "<<proc<<"\n";
-        if(time[proc]==0){time[proc] = t+1;}
+        if(time[proc]==0 || time[proc]<=t){time[proc] = t+1;}
         time[proc] += x.process_time;
       }
       else{
+       if(x.flag==0){
+          int proc=bestprocdrop(processors,x,time,t);
+          
+          if(proc==-1){not_Sch.push_back(x); continue;}
+          
+          vector<pair<int,int>> temp;
+          vector<Task> tempor;
+          for(int j=0; j<processors[proc].size(); j++){
+             if(processors[proc][j].flag==0){continue;}
+             if(processors[proc][j].start_time==-1){temp.push_back({processors[proc][j].process_time,j});}
+             else{
+                temp.push_back({processors[proc][j].end_time - t-1 , j});
+             }
+          }
+          
+          sort(temp.begin(),temp.end());
+          
+          vector<int> droped;
+          int val=0;
+          
+          for(int k=temp.size()-1; k>=0; k--){
+             val=val+temp[k].first;
+             droped.push_back(temp[k].second);
+             if(time[proc]-val + x.process_time <= x.deadline){
+               break;
+             }
+          }
+          
+          sort(droped.begin(),droped.end());
+          
+          int k=0;
+                    
+          for(int j=0; j<processors[proc].size(); j++){
+             if(k<droped.size() && j==droped[k]){cout<<"Task "<<x.tid<<" Drooped soft-hard\n";continue;}
+             tempor.push_back(processors[proc][j]);
+          }
+          tempor.push_back(x);
+          continue;
+       } 
        not_Sch.push_back(x);
       }
      }
      intask=not_Sch;
    }
    t++;
-   
-   util_matrix.push_back(utilization);
    
  }
    
@@ -146,13 +208,7 @@ void schedule_greedy1(vector<Task>& tasks, int m){
   
   cout<<"Utilization Matrix for each time slot"<<"\n";
   
-  for(int i=0; i<util_matrix.size(); i++){
-   cout<<"Time "<<i<<" ";
-    for(int j=1; j<=m; j++){
-       cout<<util_matrix[i][j] <<" ";
-    }
-    cout<<"\n";
-  }
+  
 
 }
 
