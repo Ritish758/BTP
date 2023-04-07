f836369091ca3f4bb7dbf25befb1b7f8a89452db
Aditya Trivedi <adityatrivedi25@gmail.com>
2023-04-07 09:31:27 +0530

    added results

--- Phase2/run.sh
+++ Phase2/run.sh
@@ -1,4 +1,4 @@
-FILES="./actual_data/*"
+FILES="./Tightness_variation/Random/*"
 # utils=(0.8 0.82 0.84 0.86 0.88 0.9 0.92 0.94 0.96 0.98 1)
 # dists=(2 5 10 20 50 100 200)
 for f in $FILES
--- Phase2/run3.sh
+++ Phase2/run3.sh
@@ -1,1 +1,15 @@
-echo "hello" >> outpf.txt
+FILES="./actual_data4/*"
+# utils=(0.8 0.82 0.84 0.86 0.88 0.9 0.92 0.94 0.96 0.98 1)
+# dists=(2 5 10 20 50 100 200)
+for f in $FILES
+do
+  ./random "$f" >> outpf2.txt
+  ./nearest "$f" >> outpf2.txt
+  ./baruah "$f" >> outpf2.txt
+  ./heuristic "$f" 10 0.8 >> outpf2.txt
+  ./heuristic "$f" 20 0.8 >> outpf2.txt
+  ./heuristic "$f" 10 0.9 >> outpf2.txt
+  ./heuristic "$f" 20 0.9 >> outpf2.txt
+  echo "hello" >> outpf2.txt
+  echo "processed $f at all algos"
+done
